5f2c8d701e016f73c760f38ba815c860cb20038e
Robert Krahn <robert.krahn@gmail.com>
2014-12-30 14:44:42 -0800

    clojure: editor tests

---
+++ core/lively/ide/codeeditor/modes/tests/Clojure.js
@@ -0,0 +1,4 @@
+/*global expect,it,describe,beforeEach*/
+module('lively.ide.codeeditor.modes.tests.Clojure').requires('lively.ide.codeeditor.modes.Clojure', 'lively.MochaTests').toRun(function() {
+
+}) // end of module
