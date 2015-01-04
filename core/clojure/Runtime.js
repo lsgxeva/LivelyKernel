8fe6519f2c2a38fe6d574efc4bd5b09fb926e909
Robert Krahn <robert.krahn@gmail.com>
2015-01-03 16:29:00 -0800

    clojure static analyzer

--- core/clojure/Runtime.js
+++ core/clojure/Runtime.js
@@ -313,6 +313,7 @@ Object.extend(clojure.Runtime, {
   }
 });
 
+<<<<<<< HEAD
 Object.extend(clojure.Runtime.ReplServer, {
 
     livelyLeinProfile: "{:plugins [[lein-pprint \"1.1.1\"]]\n"
@@ -461,6 +462,8 @@ Object.extend(clojure.Runtime.ReplServer, {
 
 });
 
+=======
+>>>>>>> clojure static analyzer
 clojure.StaticAnalyzer = {
   
   ensureAst: function(astOrSource) {
@@ -480,10 +483,16 @@ clojure.StaticAnalyzer = {
   },
 
   findNsForm: function(astOrSource) {
+<<<<<<< HEAD
     // clojure.StaticAnalyzer.findNsForm(that.textString)
     var ast = this.ensureAst(astOrSource);
     var nsForm = this.findFuncCallNode(ast, 'ns');
     var nsNameNode = nsForm && nsForm.children && nsForm.children.slice(1).detect(function(n) {
+=======
+    var ast = this.ensureAst(astOrSource);
+    var nsForm = this.findFuncCallNode(ast, 'ns');
+    var nsNameNode = nsForm.children.slice(1).detect(function(n) {
+>>>>>>> clojure static analyzer
       return n.type === 'symbol'; })
     return nsForm ? {
       nsName: nsNameNode ? nsNameNode.source : null,
@@ -496,6 +505,7 @@ clojure.StaticAnalyzer = {
       this.ensureAst(astOrSource), idx).last();
   },
 
+<<<<<<< HEAD
   nodeAtCursor: function(aceEd) {
     // convenience for ace
     var idx = aceEd.session.doc.positionToIndex(aceEd.getCursorPosition())
@@ -561,6 +571,16 @@ clojure.StaticAnalyzer = {
 
     return null
   }
+=======
+  createDefinitionQuery: function(astOrSource, idx) {
+    var ast = this.ensureAst(astOrSource);
+    var thing = this.nodeAtPoint(ast, idx);
+    if (!thing || !thing.source) return null;
+    var ns = this.findNsForm(ast);
+    return {nsName: ns && ns.name, ns: ns, node: thing, source: thing.source};
+  }
+
+>>>>>>> clojure static analyzer
 }
 
 }) // end of module
--- core/lively/ide/codeeditor/modes/tests/Clojure.js
+++ core/lively/ide/codeeditor/modes/tests/Clojure.js
@@ -1,4 +1,27 @@
 /*global expect,it,describe,beforeEach*/
 module('lively.ide.codeeditor.modes.tests.Clojure').requires('lively.ide.codeeditor.modes.Clojure', 'lively.MochaTests').toRun(function() {
 
+describe("Clojure static analyzer", function() {
+
+  describe("ns", function() {
+
+    it("finds and analyzes ns form", function() {
+      expect(clojure.StaticAnalyzer.findNsForm("(ns foo)\n(+ x y)"))
+      .to.containSubset({nsName: "foo"});
+    });
+
+   it("finds ns form with annotation", function() {
+      expect(clojure.StaticAnalyzer.findNsForm("(ns ^{:author \"foo\"} foo)\n(+ x y)"))
+      .to.containSubset({nsName: "foo"});
+    });
+
+  });
+
+  it("generates a find definition request for thing at point", function() {
+    expect(clojure.StaticAnalyzer.createDefinitionQuery("(ns foo)\n(map x y)", 10/*|map*/))
+          .to.containSubset({ns: {nsName: "foo"}, source: 'map'});
+  });
+
+})
+
 }) // end of module
